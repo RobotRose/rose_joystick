547773639216ec2e5c01b65bb85772b4f2a4712f
Loy van beek <beek@robot-rose.nl>
2015-03-31 09:17:32 +0200

    Doctests for JoystickInterpreter.derive_button_events pass

--- src/interpreter.py
+++ src/interpreter.py
@@ -1,3 +1,4 @@
+#! /usr/bin/env python
 from rose_joystick.srv import switch_joystick_mode, switch_joystick_modeResponse
 from rose_joystick.msg import available_modes
 
@@ -6,7 +7,7 @@ class JoystickInterpreter(object):
 
     def __init__(self):
         self.timer = None
-        pass
+        self.previous_button_states = []
 
     def start(self):
         """Start the interpreting after is was stopped"""
@@ -23,6 +24,33 @@ class JoystickInterpreter(object):
     def repeat_message(self):
         """Publish a predefined message again, e.g. to keep publishing at a certain rate even though the joystick does not update"""
         raise NotImplementedError("Subclasses must implement this function")
+
+    def derive_button_events(self, button_states):
+        """Detect and return indices of buttons that are down, were (just) released and (just) pressed
+        @param buttons a sequence of [Bool] indicating which buttons are down/pressed (True) and up (False)
+        @returns three tuples: (indices of down buttons), (indices of buttons that are just released), (indices of buttons that are just pressed).
+        >>> interpreter = JoystickInterpreter()
+        >>> interpreter.derive_button_events([False, False, False]) #First event, so nothing happens
+        ((), (), ())
+        >>> interpreter.derive_button_events([False, True, False])
+        ((1,), (), (1,))
+        >>> interpreter.derive_button_events([False, True, False]) #button at index 1 is still pressed, so not *just* pressed
+        ((1,), (), ())
+        >>> interpreter.derive_button_events([False, False, False])
+        ((), (1,), ())
+        >>> interpreter.derive_button_events([False, False, False])
+        ((), (), ())
+        """
+        if not self.previous_button_states:
+            self.previous_button_states = button_states
+
+        down = tuple([index for index, state in enumerate(button_states) if state])
+        released = tuple([index for index, (prev, curr) in enumerate(zip(self.previous_button_states, button_states)) if prev and not curr]) #If the button was pressed previously but not currently, it has been released
+        pressed = tuple([index for index, (prev, curr) in enumerate(zip(self.previous_button_states, button_states)) if not prev and curr]) #If the button was pressed previously but not currently, it has been released
+
+        self.previous_button_states = button_states
+
+        return down, released, pressed
 
     def __str__(self):
         return "Undefined JoystickInterpreter"
@@ -67,3 +95,7 @@ class CombinedInterpreter(JoystickInterpreter):
 
     def __str__(self):
         return "+".join([str(i) for i in self.subinterpreters])
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod() #Run the doctests, i.e. the """ >>> """ stuff that looks like a Python interpreter
