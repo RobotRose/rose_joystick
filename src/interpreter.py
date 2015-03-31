dee0d079256a5c9d073169a9431f106cb1c50703
Loy van beek <beek@robot-rose.nl>
2015-03-31 09:42:28 +0200

    Moved the functionality for ButtonState to its own class for more reuse

--- src/interpreter.py
+++ src/interpreter.py
@@ -7,7 +7,7 @@ class JoystickInterpreter(object):
 
     def __init__(self):
         self.timer = None
-        self.previous_button_states = []
+        self.button_state = ButtonState()
 
     def start(self):
         """Start the interpreting after is was stopped"""
@@ -24,39 +24,6 @@ class JoystickInterpreter(object):
     def repeat_message(self):
         """Publish a predefined message again, e.g. to keep publishing at a certain rate even though the joystick does not update"""
         raise NotImplementedError("Subclasses must implement this function")
-
-    def derive_button_events(self, button_states):
-        """Detect and return indices of buttons that are down, were (just) released and (just) pressed
-        @param buttons a sequence of [Bool] indicating which buttons are down/pressed (True) and up (False)
-        @returns three tuples: (indices of down buttons), (indices of buttons that are just released), (indices of buttons that are just pressed).
-        >>> interpreter = JoystickInterpreter()
-        >>> interpreter.derive_button_events([False, False, False]) #First event, so nothing happens
-        ((), (), ())
-        >>> interpreter.derive_button_events([False, True, False])
-        ((1,), (), (1,))
-        >>> interpreter.derive_button_events([False, True, False]) #button at index 1 is still pressed, so not *just* pressed
-        ((1,), (), ())
-        >>> interpreter.derive_button_events([False, False, False]) #Button 1 is no longer pressed, so released
-        ((), (1,), ())
-        >>> interpreter.derive_button_events([False, False, False]) #No changes, no buttons are pressed
-        ((), (), ())
-        >>> interpreter.derive_button_events([True, True, False])
-        ((0, 1), (), (0, 1))
-        >>> interpreter.derive_button_events([True, True, False])
-        ((0, 1), (), ())
-        >>> interpreter.derive_button_events([False, True, True])
-        ((1, 2), (0,), (2,))
-        """
-        if not self.previous_button_states:
-            self.previous_button_states = button_states
-
-        down = tuple([index for index, state in enumerate(button_states) if state])
-        released = tuple([index for index, (prev, curr) in enumerate(zip(self.previous_button_states, button_states)) if prev and not curr]) #If the button was pressed previously but not currently, it has been released
-        pressed = tuple([index for index, (prev, curr) in enumerate(zip(self.previous_button_states, button_states)) if not prev and curr]) #If the button was pressed previously but not currently, it has been released
-
-        self.previous_button_states = button_states
-
-        return down, released, pressed
 
     def __str__(self):
         return "Undefined JoystickInterpreter"
@@ -101,6 +68,44 @@ class CombinedInterpreter(JoystickInterpreter):
 
     def __str__(self):
         return "+".join([str(i) for i in self.subinterpreters])
+
+class ButtonState(object):
+    def __init__(self):
+        self.previous_button_states = []
+
+    def derive_button_events(self, button_states):
+        """Detect and return indices of buttons that are down, were (just) released and (just) pressed
+        @param buttons a sequence of [Bool] indicating which buttons are down/pressed (True) and up (False)
+        @returns three tuples: (indices of down buttons), (indices of buttons that are just released), (indices of buttons that are just pressed).
+        >>> state = ButtonState()
+        >>> state.derive_button_events([False, False, False]) #First event, so nothing happens
+        ((), (), ())
+        >>> state.derive_button_events([False, True, False])
+        ((1,), (), (1,))
+        >>> state.derive_button_events([False, True, False]) #button at index 1 is still pressed, so not *just* pressed
+        ((1,), (), ())
+        >>> state.derive_button_events([False, False, False]) #Button 1 is no longer pressed, so released
+        ((), (1,), ())
+        >>> state.derive_button_events([False, False, False]) #No changes, no buttons are pressed
+        ((), (), ())
+        >>> state.derive_button_events([True, True, False])
+        ((0, 1), (), (0, 1))
+        >>> state.derive_button_events([True, True, False])
+        ((0, 1), (), ())
+        >>> state.derive_button_events([False, True, True])
+        ((1, 2), (0,), (2,))
+        """
+        if not self.previous_button_states:
+            self.previous_button_states = button_states
+
+        down = tuple([index for index, state in enumerate(button_states) if state])
+        released = tuple([index for index, (prev, curr) in enumerate(zip(self.previous_button_states, button_states)) if prev and not curr]) #If the button was pressed previously but not currently, it has been released
+        pressed = tuple([index for index, (prev, curr) in enumerate(zip(self.previous_button_states, button_states)) if not prev and curr]) #If the button was pressed previously but not currently, it has been released
+
+        self.previous_button_states = button_states
+
+        return down, released, pressed
+
 
 if __name__ == "__main__":
     import doctest
